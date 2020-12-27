220f17736aee52a83e6c9c8d579d97088ca1d63c
Joey Wong <38258708+a7e6j2@users.noreply.github.com>
2020-12-27 14:39:42 +0800

    Update rules.php
    add put verb

--- config/rules.php
+++ config/rules.php
@@ -8,7 +8,7 @@ return [
 
     'POST service/create' => 'service/create',
 
-    'POST service/update/{id}' => 'service/update',
+    'PUT,POST service/update/{id}' => 'service/update',
 
     'GET service/{id}' => 'service/view',
 
